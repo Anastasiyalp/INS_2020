864d8226738157fcf00b418d6e8cbee22b76869d
Liashchevskaia <295341@bk.ru>
2020-04-24 18:54:30 +0300

    del trash

--- prac7_3.py
+++ prac7_3.py
@@ -28,7 +28,6 @@ test_data, test_res = data[train_size+val_size:], res[train_size+val_size:]
 model = Sequential()
 model.add(layers.GRU(32, recurrent_activation='sigmoid', input_shape=(None, 1), return_sequences=True))
 model.add(layers.LSTM(32, activation='relu', input_shape=(None, 1), return_sequences=True, dropout=0.2))
-#model.add(Dropout(0.5))
 model.add(layers.GRU(32, input_shape=(None, 1), recurrent_dropout=0.2))
 model.add(layers.Dense(1))
 
